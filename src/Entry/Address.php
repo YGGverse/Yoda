0bb3cf2415f9d49c3ed5a10883976a64e9fe2d51
yggverse <yggverse@project>
2024-04-09 13:56:09 +0300

    add placeholder text

--- src/Entry/Address.php
+++ src/Entry/Address.php
@@ -9,12 +9,17 @@ class Address
     public \GtkEntry $entry;
 
     public function __construct(
-        ?string $value = null
+        ?string $text = null,
+        ?string $placeholder = 'URL or any search term...'
     ) {
         $this->entry = new \GtkEntry();
 
         $this->entry->set_text(
-            $value
+            $text
+        );
+
+        $this->entry->set_placeholder_text(
+            $placeholder
         );
 
         $this->entry->set_max_length(
