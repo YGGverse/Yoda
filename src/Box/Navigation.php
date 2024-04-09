b48b3a6980d3f08b98036ff5137df68980de627d
yggverse <yggverse@project>
2024-04-09 15:13:04 +0300

    draft navigation button

--- src/Box/Navigation.php
+++ src/Box/Navigation.php
@@ -9,6 +9,7 @@ class Navigation
     public \GtkBox $box;
 
     public \Yggverse\Yoda\Entry\Address $address;
+    public \Yggverse\Yoda\Button\Go $go;
 
     public function __construct(
         string $name = 'boxNavigation'
@@ -16,7 +17,7 @@ class Navigation
         global $config;
 
         $this->box = new \GtkBox(
-            \GtkOrientation::VERTICAL
+            \GtkOrientation::HORIZONTAL
         );
 
         $this->box->set_name(
@@ -27,8 +28,23 @@ class Navigation
             $config->homepage
         );
 
-        $this->box->add(
-            $this->address->entry
+        $this->box->pack_start(
+            $this->address->entry,
+            true,
+            true,
+            8
         );
+
+        if ($config->interface->window->navigation->button->go->enabled)
+        {
+            $this->go = new \Yggverse\Yoda\Button\Go();
+
+            $this->box->pack_end(
+                $this->go->button,
+                false,
+                false,
+                8
+            );
+        }
     }
 }
---
+++ src/Button/Go.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yggverse\Yoda\Button;
+
+class Go
+{
+    public \GtkButton $button;
+
+    public function __construct(
+        ?string $label = 'Go'
+    ) {
+        $this->button = \GtkButton::new_with_label(
+            $label
+        );
+    }
+}
