95abe8154786b20fc9fb8a61838f3b55769f73a0
yggverse <yggverse@project>
2024-04-09 11:25:22 +0300

    init memory model

--- src/Model/Memory.php
+++ src/Model/Memory.php
@@ -6,6 +6,23 @@ namespace Yggverse\Yoda\Model;
 
 class Memory
 {
+    private array $_memory = [];
+
     public function __construct()
     {}
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->_memory[$key] = $value;
+    }
+
+    public function get(string $key): mixed
+    {
+        if (isset($this->_memory[$key]))
+        {
+            return $this->_memory[$key];
+        }
+
+        return null;
+    }
 }
