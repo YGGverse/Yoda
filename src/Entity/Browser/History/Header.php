c5b62addd4cf95c0956e0c12c8f1b8b1b5c34812
yggverse <yggverse@project>
2024-07-06 03:29:39 +0300

    remove HeaderBar abstraction

--- src/Entity/Browser/History/Header.php
+++ src/Entity/Browser/History/Header.php
@@ -4,7 +4,43 @@ declare(strict_types=1);
 
 namespace Yggverse\Yoda\Entity\Browser\History;
 
-class Header extends \Yggverse\Yoda\Abstract\Entity\HeaderBar
+class Header
 {
+    public \GtkHeaderBar $gtk;
+
+    protected bool $_actions = true;
     protected string $_title = 'History - Yoda';
+    protected string $_subtitle = '';
+
+    public function __construct()
+    {
+        $this->gtk = new \GtkHeaderBar;
+
+        $this->gtk->set_show_close_button(
+            $this->_actions
+        );
+
+        $this->gtk->set_title(
+            $this->_title
+        );
+
+        $this->gtk->set_subtitle(
+            $this->_subtitle
+        );
+    }
+
+    public function setTitle(
+        ?string $title = null
+    ): void
+    {
+        $this->gtk->set_title(
+            is_null($title) ? $this->_title : sprintf(
+                '%s - %s',
+                trim(
+                    $title
+                ),
+                $this->_title
+            )
+        );
+    }
 }
