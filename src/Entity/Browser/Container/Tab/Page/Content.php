a71a4a66ce8b7ec8ac638c185a0e05a9c07e5d35
yggverse <yggverse@project>
2024-07-06 22:24:04 +0300

    group margin elements

--- src/Entity/Browser/Container/Tab/Page/Content.php
+++ src/Entity/Browser/Container/Tab/Page/Content.php
@@ -31,16 +31,16 @@ class Content
             $this->_margin
         );
 
+        $this->gtk->set_margin_end(
+            $this->_margin
+        );
+
         $this->gtk->set_propagate_natural_height(
             true
         );
 
         $this->gtk->set_propagate_natural_width(
             true
-        );
-
-        $this->gtk->set_margin_end(
-            $this->_margin
         );
 
         // Init label
