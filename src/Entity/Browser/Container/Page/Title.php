55763a643d7ec4a17c031700f052bf877cabcc72
yggverse <yggverse@project>
2024-07-09 08:38:06 +0300

    add tab tooltips

--- src/Entity/Browser/Container/Page/Title.php
+++ src/Entity/Browser/Container/Page/Title.php
@@ -18,6 +18,7 @@ class Title
     private int $_length = 16;
     private ?string $_value = 'New page';
     private ?string $_subtitle = null;
+    private ?string $_tooltip = null;
 
     public function __construct(
         \Yggverse\Yoda\Entity\Browser\Container\Page $page,
@@ -41,12 +42,19 @@ class Title
 
     public function setValue(
         ?string $value = null,
-        ?string $subtitle = null
+        ?string $subtitle = null,
+        ?string $tooltip = null,
     ): void
     {
         $this->gtk->set_text(
             is_null($value) ? $this->_value : trim(
                 $value
+            )
+        );
+
+        $this->gtk->set_tooltip_text(
+            is_null($tooltip) ? $value : trim(
+                $tooltip
             )
         );
 
@@ -68,6 +76,17 @@ class Title
         );
     }
 
+    public function setTooltip(
+        ?string $tooltip = null
+    ): void
+    {
+        $this->gtk->set_subtitle(
+            is_null($tooltip) ? $this->_tooltip : trim(
+                $tooltip
+            )
+        );
+    }
+
     public function getValue(): ?string
     {
         return $this->gtk->get_text();
@@ -76,5 +95,10 @@ class Title
     public function getSubtitle(): ?string
     {
         return $this->gtk->get_subtitle();
+    }
+
+    public function getTooltip(): ?string
+    {
+        return $this->gtk->get_tooltip();
     }
 }
