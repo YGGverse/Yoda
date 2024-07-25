bd6b544c765701955d3c5be62f68a3b503850e8a
yggverse <yggverse@project>
2024-07-26 02:08:12 +0300

    use local phpcpp build

--- INSTALL.sh
+++ INSTALL.sh
@@ -206,6 +206,9 @@ sed -i "/EXTENSION_DIR       =   /c\
 sed -i "/INI_DIR     =   /c\
          INI_DIR     =   /dev/null" "$DIR_PHP_GTK_SOURCE/Makefile"
 
+## Use local builds
+sed -i "s|-lphpcpp|-Wl,-rpath=\"$DIR_PHP_CPP_TARGET/lib\" -lphpcpp|g" "$DIR_PHP_GTK_SOURCE/Makefile"
+
 ## Force build for new installation
 if [ ! -f "$("$DIR_PHP_SRC_TARGET/bin/php-config" --extension-dir)/php-gtk3.so" ]; then BUILD_PHP_GTK="y"
 else # or ask for re-build
