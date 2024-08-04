d9708a32626b181a545b4664d617ebb1ee7ac0b6
yggverse <yggverse@project>
2024-08-04 19:45:28 +0300

    init main component

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "main.h"
+
+void activate(
+    GtkApplication *application
+) {
+    // Init default component
+    new YodaBrowser(
+        application
+    );
+}
+
+int main(
+    int argc,
+    char *argv[]
+) {
+    // Create a new application
+    GtkApplication *application = gtk_application_new(
+        NULL,
+        G_APPLICATION_DEFAULT_FLAGS
+    );
+
+    // Connect the "activate" signal to the callback function
+    g_signal_connect(
+        application,
+        "activate",
+        G_CALLBACK(
+            activate
+        ),
+        NULL
+    );
+
+    // Run the application
+    int status = g_application_run(
+        G_APPLICATION(
+            application
+        ),
+        argc,
+        argv
+    );
+
+    // Clean up
+    g_object_unref(
+        application
+    );
+
+    // Result
+    return status;
+}
---
+++ src/main.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <gtk/gtk.h>
+
+#include "Yoda/Browser.h"
+
+void static activate(
+    GtkApplication *application
+);
+
+int main(
+    int argc,
+    char *argv[]
+);
