c477c70c5618e93b04b67952ef16e0fcea4ca625
yggverse <yggverse@project>
2024-08-11 14:41:56 +0300

    use sql

--- src/lib/database/session.cpp
+++ src/lib/database/session.cpp
@@ -7,14 +7,14 @@ Session::Session(
 ) {
     status = sqlite3_exec(
         connection,
-        R"(
+        R"SQL(
             CREATE TABLE IF NOT EXISTS `session`
             (
                 `id`      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 `time`    INTEGER NOT NULL,
                 `request` VARCHAR(1024)
             )
-        )",
+        )SQL",
         nullptr,
         nullptr,
         &error
