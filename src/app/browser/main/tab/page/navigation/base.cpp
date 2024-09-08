6d93fd9b0128a0e3e74399e325a18fbb872ac5b9
yggverse <yggverse@project>
2024-09-08 07:27:19 +0300

    remove deprecated construction

--- src/app/browser/main/tab/page/navigation/base.cpp
+++ src/app/browser/main/tab/page/navigation/base.cpp
@@ -4,10 +4,6 @@ using namespace app::browser::main::tab::page::navigation;
 
 Base::Base()
 {
-    set_action_name(
-        "tab.base"
-    );
-
     set_icon_name(
         "go-home-symbolic"
     );
