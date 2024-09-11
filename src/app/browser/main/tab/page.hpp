ce45323072e2a34ed2d89bf04fe4c4a302e505af
yggverse <yggverse@project>
2024-09-12 01:26:06 +0300

    update comment

--- src/app/browser/main/tab/page.hpp
+++ src/app/browser/main/tab/page.hpp
@@ -87,7 +87,7 @@ namespace app::browser::main::tab
             // Tools
             double progress_fraction; // Page preparation status, wanted for following needs:
                                       // * indication widgets (progress bar)
-                                      // * toggle window update action in main thread (e.g. on async data loading completed)
+                                      // * toggle window update action in main thread (e.g. on async data loading complete)
 
             // Actions
             Glib::RefPtr<Gio::SimpleAction> action__update;
