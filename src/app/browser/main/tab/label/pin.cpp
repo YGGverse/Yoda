6f77138796565bf0bad5297bb830e3204fc4c687
yggverse <yggverse@project>
2024-09-17 07:10:28 +0300

    implement pin widget

---
+++ src/app/browser/main/tab/label/pin.cpp
@@ -0,0 +1,10 @@
+#include "pin.hpp"
+
+using namespace app::browser::main::tab::label;
+
+Pin::Pin()
+{
+    set_from_icon_name(
+        "view-pin-symbolic"
+    );
+}
---
+++ src/app/browser/main/tab/label/pin.hpp
@@ -0,0 +1,19 @@
+#ifndef APP_BROWSER_MAIN_TAB_LABEL_PIN_HPP
+#define APP_BROWSER_MAIN_TAB_LABEL_PIN_HPP
+
+#include <gtkmm/image.h>
+
+namespace app::browser::main::tab::label
+{
+    class Pin : public Gtk::Image
+    {
+        /*
+         * Class API
+         */
+        public:
+
+            Pin();
+    };
+}
+
+#endif // APP_BROWSER_MAIN_TAB_LABEL_PIN_HPP
