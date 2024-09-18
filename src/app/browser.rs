ec4f6095f2dff222a155a27fc9ca607dd93bb971
yggverse <yggverse@project>
2024-09-18 21:22:00 +0300

    enable titlebar feature

--- src/app/browser.rs
+++ src/app/browser.rs
@@ -9,7 +9,7 @@ pub fn new(app: &Application) -> ApplicationWindow
     return ApplicationWindow::builder().application(app)
                                        .default_width(640)
                                        .default_height(480)
-                                     //.titlebar(&header::new())
+                                       .titlebar(&header::new())
                                        .child(&main::new())
                                        .build();
 }
