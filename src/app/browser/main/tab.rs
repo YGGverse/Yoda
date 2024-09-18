9e27fb01658a24451999e2af875cd85935c45b27
yggverse <yggverse@project>
2024-09-18 22:47:53 +0300

    setup appended new page

--- src/app/browser/main/tab.rs
+++ src/app/browser/main/tab.rs
@@ -13,10 +13,28 @@ pub fn append(
     is_current : bool
 ) -> u32
 {
-    return tab.append_page(
-        &page::new(),
+    let page = page::new();
+
+    let page_number = tab.append_page(
+        &page,
         Some(
             &label::new()
         )
     );
+
+    tab.set_tab_reorderable(
+        &page,
+        true
+    );
+
+    if is_current
+    {
+        tab.set_current_page(
+            Some(
+                page_number
+            )
+        );
+    }
+
+    return page_number;
 }
