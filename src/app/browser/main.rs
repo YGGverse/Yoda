9039c7b2c932e66f0add0498008480c19fe07c0f
yggverse <yggverse@project>
2024-09-18 22:07:52 +0300

    init main tab container

--- src/app/browser/main.rs
+++ src/app/browser/main.rs
@@ -1,8 +1,17 @@
+#[path = "main/tab.rs"] mod tab;
+
 use gtk::Box;
+use gtk::prelude::BoxExt;
 
 pub fn new() -> Box
 {
-    return Box::builder().orientation(
+    let main = Box::builder().orientation(
         gtk::Orientation::Vertical
     ).build();
+
+    main.append(
+        &tab::new()
+    );
+
+    return main;
 }
---
+++ src/app/browser/main/tab.rs
@@ -0,0 +1,6 @@
+use gtk::Notebook;
+
+pub fn new() -> Notebook
+{
+    return Notebook::builder().build();
+}
