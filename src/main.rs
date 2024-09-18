0f81beb26b6242662b2d82706bcb01680de1e0ad
yggverse <yggverse@project>
2024-09-18 16:24:25 +0300

    init main application window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+fn main() -> glib::ExitCode
+{
+    let app = Application::builder()
+        .application_id("io.github.yggverse.Yoda.app")
+        .build();
+
+    app.connect_activate(
+        |app|
+        {
+            let window = ApplicationWindow::builder()
+                .application(app)
+                .default_width(640)
+                .default_height(480)
+                .title("Yoda")
+                .build();
+
+            window.present();
+        }
+    );
+
+    app.run()
 }
