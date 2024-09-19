ab3a815b7161cc96f3bd473355ecb9cc25b419c1
yggverse <yggverse@project>
2024-09-19 22:19:28 +0300

    add tab submenu

--- src/app/browser/header/tray/menu.rs
+++ src/app/browser/header/tray/menu.rs
@@ -15,6 +15,18 @@ pub fn new() -> MenuButton
 
     let model = gio::Menu::new();
 
+        let model_tab = gio::Menu::new();
+
+            model_tab.append(
+                Some("Append"),
+                Some("win.tab_append")
+            );
+
+        model.append_submenu(
+            Some("Tab"),
+            &model_tab
+        );
+
         model.append(
             Some("Debug"),
             Some("win.debug")
