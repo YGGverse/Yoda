ecd686976cb582bb506b21d8007fab3c1e20f350
yggverse <yggverse@project>
2024-09-22 02:32:57 +0300

    define struct, implement action

--- src/browser/header/tray/mod.rs
+++ src/browser/header/tray/mod.rs
@@ -5,14 +5,18 @@ use gtk::prelude::BoxExt;
 use gtk::Box;
 
 pub fn new() -> Box {
+    // Init components
+    let tab = tab::new();
+
+    // Init widget
     let tray = Box::builder()
         .orientation(gtk::Orientation::Horizontal)
         .spacing(8)
         .build();
 
     // Compose childs
-    tray.append(&menu::new());
-    tray.append(&tab::new());
+    tray.append(&menu::new()); // @TODO
+    tray.append(tab.widget.as_ref());
 
-    tray
+    tray // @TODO struct
 }
--- src/browser/header/tray/tab/mod.rs
+++ src/browser/header/tray/tab/mod.rs
@@ -1,10 +1,27 @@
+use std::sync::Arc;
+
+use gtk::prelude::{ButtonExt, WidgetExt};
 use gtk::Button;
 
-pub fn new() -> Button {
-    let tab = Button::builder()
-        .icon_name("tab-new-symbolic")
-        .tooltip_text("New tab")
-        .build();
+pub struct Tab {
+    pub widget: Arc<gtk::Button>,
+}
 
-    return tab;
+pub fn new() -> Tab {
+    // Init widget
+    let widget = Arc::new(
+        Button::builder()
+            .icon_name("tab-new-symbolic")
+            .tooltip_text("New tab")
+            .build(),
+    );
+
+    // Init events
+    widget.connect_clicked(|this| {
+        this.activate_action("win.tab_append", None)
+            .expect("The action does not exist");
+    });
+
+    // Result
+    Tab { widget }
 }
