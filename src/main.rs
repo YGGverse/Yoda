b3faf9a1cb91abe47d946b487349b564038cd8a7
yggverse <yggverse@project>
2024-09-21 23:28:46 +0300

    reorder dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,9 @@ use gtk::prelude::{ApplicationExt, ApplicationExtManual, GtkApplicationExt, GtkW
 
 use gtk::{glib, Application};
 
+const APP_ID: &str = "io.github.yggverse.Yoda";
+
 fn main() -> glib::ExitCode {
-    // Init meta
-    const APP_ID: &str = "io.github.yggverse.Yoda";
-
-    // Init profile directory
-    let mut fs = gtk::glib::user_config_dir();
-
-    fs.push(APP_ID);
-
-    if let Err(e) = fs::create_dir_all(&fs) {
-        panic!("Failed to create profile directory: {e}")
-    }
-
-    // Init profile database
-    let mut db = fs.clone();
-
-    db.push("database.sqlite3");
-
-    let db = match sqlite::open(db) {
-        Ok(db) => db,
-        Err(e) => panic!("Failed to connect profile database: {e}"),
-    };
-
     // Init app
     let app = Application::builder().application_id(APP_ID).build();
 
@@ -39,8 +19,29 @@ fn main() -> glib::ExitCode {
     app.set_accels_for_action("win.quit", &["<Ctrl>Escape"]);
 
     // Create new window
-    app.connect_activate(move |app| {
-        browser::new(&app, &db, 640, 480).present();
+    app.connect_activate({
+        // Init profile directory
+        let mut fs = gtk::glib::user_config_dir();
+
+        fs.push(APP_ID);
+
+        if let Err(e) = fs::create_dir_all(&fs) {
+            panic!("Failed to create profile directory: {e}")
+        }
+
+        // Init profile database
+        let mut db = fs.clone();
+
+        db.push("database.sqlite3");
+
+        let db = match sqlite::open(db) {
+            Ok(db) => db,
+            Err(e) => panic!("Failed to connect profile database: {e}"),
+        };
+
+        move |this| {
+            browser::new(&this, &db, 640, 480).present();
+        }
     });
 
     // Start
