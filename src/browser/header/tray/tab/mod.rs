4877e240206ef45887363917bedca2fe8894a854
yggverse <yggverse@project>
2024-09-23 00:25:56 +0300

    connect events

--- src/browser/header/tray/tab/mod.rs
+++ src/browser/header/tray/tab/mod.rs
@@ -5,19 +5,11 @@ pub struct Tab {
 }
 
 impl Tab {
+    // Construct
     pub fn new() -> Tab {
-        // Init widget
-        let widget = widget::Tab::new();
-
-        // Init events
-        /* @TODO
-        widget.connect_clicked(|this| {
-            this.activate_action("win.tab_append", None)
-                .expect("The action does not exist");
-        }); */
-
-        // Result
-        Self { widget }
+        Self {
+            widget: widget::Tab::new(),
+        }
     }
 
     // Getters
--- src/browser/header/tray/tab/widget.rs
+++ src/browser/header/tray/tab/widget.rs
@@ -1,3 +1,5 @@
+use gtk::prelude::{ButtonExt, WidgetExt};
+
 pub struct Tab {
     gtk: gtk::Button,
 }
@@ -5,10 +7,17 @@ pub struct Tab {
 impl Tab {
     // Construct
     pub fn new() -> Tab {
+        // Init widget
         let gtk = gtk::Button::builder()
             .icon_name("tab-new-symbolic")
             .tooltip_text("New tab")
             .build();
+
+        // Init events
+        gtk.connect_clicked(|this| {
+            this.activate_action("win.tab_append", None)
+                .expect("The action does not exist");
+        });
 
         Self { gtk }
     }
