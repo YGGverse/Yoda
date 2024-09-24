5a1f10285bfb48b43a42528042626b56df9244d7
yggverse <yggverse@project>
2024-09-24 22:33:37 +0300

    add tab page menu items

--- src/browser/header/tray/menu/mod.rs
+++ src/browser/header/tray/menu/mod.rs
@@ -11,6 +11,19 @@ impl Menu {
         model_tab.append(Some("New"), Some("win.tab_append"));
         model_tab.append(Some("Pin"), Some("win.tab_pin"));
 
+        let model_tab_page = gio::Menu::new();
+        model_tab_page.append(Some("Base"), Some("win.tab_page_base"));
+
+        let model_tab_page_history = gio::Menu::new();
+        model_tab_page_history.append(Some("Back"), Some("win.tab_page_history_back"));
+        model_tab_page_history.append(Some("Forward"), Some("win.tab_page_history_forward"));
+        model_tab_page.append_submenu(Some("History"), &model_tab_page_history);
+
+        model_tab_page.append(Some("Reload"), Some("win.tab_page_reload"));
+        model_tab_page.append(Some("Bookmark"), Some("win.tab_page_bookmark"));
+
+        model_tab.append_submenu(Some("Page"), &model_tab_page);
+
         let model_tab_close = gio::Menu::new();
         model_tab_close.append(Some("Current"), Some("win.tab_close"));
         model_tab_close.append(Some("All"), Some("win.tab_close_all"));
