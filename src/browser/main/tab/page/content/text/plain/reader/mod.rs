f00d652a9109ae561d1b39e3a9051ecafebb86f9
yggverse <yggverse@project>
2024-09-25 18:41:19 +0300

    draft plain text reader widget

---
+++ src/browser/main/tab/page/content/text/plain/reader/mod.rs
@@ -0,0 +1,27 @@
+use gtk::{Align, Label};
+
+pub struct Reader {
+    widget: Label,
+}
+
+impl Reader {
+    // Construct
+    pub fn new() -> Reader {
+        Self {
+            widget: Label::builder()
+                .halign(Align::Start)
+                .valign(Align::Start)
+                .margin_start(8)
+                .margin_end(8)
+                .wrap(true)
+                .selectable(true)
+                .use_markup(true)
+                .build(),
+        }
+    }
+
+    // Getters
+    pub fn widget(&self) -> &Label {
+        &self.widget
+    }
+}
