4b3ea84f398c0a2af23b8a6c2d352c0a032debae
yggverse <yggverse@project>
2024-09-28 13:07:26 +0300

    add page status enum

--- src/browser/main/tab/page/meta.rs
+++ src/browser/main/tab/page/meta.rs
@@ -1,14 +1,30 @@
 use gtk::glib::GString;
 
+// Page MIME type (not related with gemini status code)
+// Useful for content renderer detection, etc
 pub enum Mime {
     TextGemini,
     TextPlain,
 }
 
+// Internal page status (not related with gemini status code)
+// Useful for widgets composition
+pub enum Status {
+    Failure,
+    Redirect,
+    Success,
+}
+
 pub struct Meta {
+    // Text meta data for page
+    // Useful to update window title, label text, etc
     pub title: Option<GString>,
     pub description: Option<GString>,
+    // Enums
     pub mime: Option<Mime>,
+    pub status: Option<Status>,
+    // Useful to compose other widgets
+    // (e.g. navigation bar listen for this value update)
     pub progress_fraction: f32,
 }
 
@@ -18,6 +34,7 @@ impl Meta {
             title: None,
             description: None,
             mime: None,
+            status: None,
             progress_fraction: 0.0,
         }
     }
