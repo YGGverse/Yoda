166f07aef88534a185f160f12de2f69655e152df
yggverse <yggverse@project>
2024-10-06 05:55:14 +0300

    update argument name

--- src/app/browser/header/widget.rs
+++ src/app/browser/header/widget.rs
@@ -6,10 +6,10 @@ pub struct Widget {
 
 impl Widget {
     // Construct
-    pub fn new(tray: &Box, title_widget: Option<&Box>) -> Self {
+    pub fn new(pack_start: &Box, title_widget: Option<&Box>) -> Self {
         let gobject = HeaderBar::builder().build();
 
-        gobject.pack_start(tray);
+        gobject.pack_start(pack_start);
         gobject.set_title_widget(title_widget);
 
         Self { gobject }
