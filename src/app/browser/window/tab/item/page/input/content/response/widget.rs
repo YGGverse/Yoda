d1c8afd83f95757649fe322c9443122eff6e4277
yggverse <yggverse@project>
2024-10-16 08:41:17 +0300

    grab focus on set

--- src/app/browser/window/tab/item/page/input/content/response/widget.rs
+++ src/app/browser/window/tab/item/page/input/content/response/widget.rs
@@ -11,7 +11,7 @@ pub struct Widget {
 impl Widget {
     // Construct
     pub fn new_arc() -> Arc<Self> {
-        let gobject = Entry::builder().hexpand(true).build();
+        let gobject = Entry::builder().editable(true).hexpand(true).build();
 
         Arc::new(Self { gobject })
     }
@@ -20,7 +20,8 @@ impl Widget {
     pub fn set(&self, placeholder_text: &str, sensitive: bool) {
         self.gobject.set_text(&""); // reset
         self.gobject.set_placeholder_text(Some(placeholder_text));
-        self.gobject.set_sensitive(sensitive);
+        // self.gobject.set_sensitive(sensitive);
+        self.gobject.grab_focus();
     }
 
     // Getters
