0a506819451a58e764a6331fe5a5f4bec18119ce
yggverse <yggverse@project>
2024-11-01 17:40:50 +0200

    use ContentFit::ScaleDown for picture browser

--- src/app/browser/window/tab/item/page/content/image.rs
+++ src/app/browser/window/tab/item/page/content/image.rs
@@ -1,4 +1,4 @@
-use gtk::{gdk_pixbuf::Pixbuf, prelude::WidgetExt, Picture};
+use gtk::{gdk_pixbuf::Pixbuf, prelude::WidgetExt, ContentFit, Picture};
 
 pub struct Image {
     gobject: Picture,
@@ -7,13 +7,15 @@ pub struct Image {
 impl Image {
     // Defaults
 
-    const DEFAULT_MARGIN: i32 = 6;
+    pub const DEFAULT_MARGIN: i32 = 6;
+    pub const DEFAULT_CONTENT_FIT: ContentFit = ContentFit::ScaleDown;
 
     // Constructors
 
     pub fn new_from_pixbuf(buffer: &Pixbuf) -> Self {
         let gobject = Picture::for_pixbuf(buffer);
 
+        gobject.set_content_fit(Self::DEFAULT_CONTENT_FIT);
         gobject.set_margin_end(Self::DEFAULT_MARGIN);
         gobject.set_margin_start(Self::DEFAULT_MARGIN);
 
