921f8d412e240baed6d94767fc3c0567befeb6a8
yggverse <yggverse@project>
2024-11-16 09:47:25 +0200

    update field set

--- src/profile/identity/database.rs
+++ src/profile/identity/database.rs
@@ -11,12 +11,11 @@ pub fn init(tx: &Transaction) -> Result<usize, Error> {
     tx.execute(
         "CREATE TABLE IF NOT EXISTS `profile_identity`
         (
-            `id`          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            `profile_id`  INTEGER NOT NULL,
-            `time`        INTEGER NOT NULL,
-            `name`        VARCHAR(255),
-            `certificate` TEXT NOT NULL,
-            `key`         TEXT NOT NULL
+            `id`         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `profile_id` INTEGER NOT NULL,
+            `time`       INTEGER NOT NULL,
+            `name`       VARCHAR(255),
+            `pem`        TEXT NOT NULL
         )",
         [],
     )
