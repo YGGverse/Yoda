326a0d088752e0bcd1dd0b2edd558a940c558a2e
yggverse <yggverse@project>
2024-11-16 21:38:32 +0200

    add css class for action button

--- src/app/browser/window/tab/item/page/content/status/identity.rs
+++ src/app/browser/window/tab/item/page/content/status/identity.rs
@@ -6,12 +6,14 @@ const DEFAULT_ICON_NAME: &str = "avatar-default-symbolic";
 const DEFAULT_TITLE: &str = "Identity";
 const DEFAULT_DESCRIPTION: &str = "Client certificate required to continue!";
 const DEFAULT_BUTTON_LABEL: &str = "Select";
+const DEFAULT_BUTTON_CLASS: &str = "suggested-action";
 
 /// Create new default preset for `Identity`
 /// [StatusPage](https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/class.StatusPage.html)
 pub fn new_gobject() -> StatusPage {
     // Init certificate selection
     let button = &Button::builder()
+        .css_classes([DEFAULT_BUTTON_CLASS])
         .label(DEFAULT_BUTTON_LABEL)
         .halign(Align::Center)
         .build();
