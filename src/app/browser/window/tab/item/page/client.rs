c728d1c001fc38faab2cab8232bf0656193d95a9
yggverse <yggverse@project>
2024-11-27 19:21:09 +0200

    add comments

--- src/app/browser/window/tab/item/page/client.rs
+++ src/app/browser/window/tab/item/page/client.rs
@@ -1,5 +1,11 @@
+/// Single client instance holder for `Page` object.
+///
+/// unlike new client instance init on every page open,
+/// this solution provide additional client-side features
+/// e.g. session cache management on certificate change in runtime
 pub struct Client {
     pub gemini: gemini::Client,
+    // other supported clients here..
 }
 
 impl Client {
