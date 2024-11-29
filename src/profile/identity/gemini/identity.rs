82ea70b53859dc9b224f5e057dae06c0d27141f5
yggverse <yggverse@project>
2024-11-29 16:27:14 +0200

    add comments

--- src/profile/identity/gemini/identity.rs
+++ src/profile/identity/gemini/identity.rs
@@ -3,12 +3,15 @@ use error::Error;
 
 use gtk::gio::TlsCertificate;
 
+/// Gemini identity holder for cached record in application-wide struct format.
+/// Implements also additional conversion methods.
 pub struct Identity {
     pub pem: String,
     // pub scope: String,
 }
 
 impl Identity {
+    /// Convert `Self` to [TlsCertificate](https://docs.gtk.org/gio/class.TlsCertificate.html)
     pub fn to_tls_certificate(&self) -> Result<TlsCertificate, Error> {
         match TlsCertificate::from_pem(&self.pem) {
             Ok(certificate) => Ok(certificate),
