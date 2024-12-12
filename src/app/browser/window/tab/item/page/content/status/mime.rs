e53a4cb2472f279a55866077e197d881cb55cfed
yggverse <yggverse@project>
2024-12-12 12:15:32 +0200

    change icon

--- src/app/browser/window/tab/item/page/content/status/mime.rs
+++ src/app/browser/window/tab/item/page/content/status/mime.rs
@@ -1,7 +1,7 @@
 use adw::StatusPage;
 
 const DEFAULT_TITLE: &str = "Oops";
-const DEFAULT_ICON_NAME: &str = "help-contents-symbolic";
+const DEFAULT_ICON_NAME: &str = "dialog-question-symbolic";
 
 /// Create new default `GObject` preset for mime issue
 /// [StatusPage](https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/class.StatusPage.html)
