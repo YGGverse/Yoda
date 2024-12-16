bfe4048eb10e6fc618c409dff6da53538764f7bb
yggverse <yggverse@project>
2024-12-16 15:55:47 +0200

    enshort rgba values

--- src/app/browser/window/tab/item/page/content/text/search/tag/current.rs
+++ src/app/browser/window/tab/item/page/content/text/search/tag/current.rs
@@ -2,7 +2,7 @@ use gtk::{gdk::RGBA, TextTag};
 
 pub fn new() -> TextTag {
     TextTag::builder()
-        .background_rgba(&RGBA::new(0.0, 0.3906, 0.8653, 1.0)) // @TODO use accent colors after adw 1.6 update
+        .background_rgba(&RGBA::new(0.0, 0.4, 0.9, 1.0)) // @TODO use accent colors after adw 1.6 update
         .foreground_rgba(&RGBA::new(1.0, 1.0, 1.0, 1.0))
         .build()
 }
