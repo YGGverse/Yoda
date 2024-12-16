5240aa1879601b4a0970344fbc2135b048244387
yggverse <yggverse@project>
2024-12-16 14:40:25 +0200

    change current tag decoration colors

--- src/app/browser/window/tab/item/page/content/text/search/tag/current.rs
+++ src/app/browser/window/tab/item/page/content/text/search/tag/current.rs
@@ -2,6 +2,7 @@ use gtk::{gdk::RGBA, TextTag};
 
 pub fn new() -> TextTag {
     TextTag::builder()
-        .background_rgba(&RGBA::new(0.502, 0.502, 0.502, 0.5)) // @TODO
+        .background_rgba(&RGBA::new(0.0, 0.0, 0.0, 1.0))
+        .foreground_rgba(&RGBA::new(1.0, 1.0, 1.0, 1.0))
         .build()
 }
