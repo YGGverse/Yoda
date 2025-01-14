44be1a7dab2a3921648ebf977fcd88cb9cbccc39
yggverse <yggverse@project>
2025-01-14 05:10:38 +0200

    remove extra reference

--- src/app/browser/window/tab/item/page.rs
+++ src/app/browser/window/tab/item/page.rs
@@ -237,8 +237,7 @@ impl Page {
                         let title = "Titan input";
 
                         // Toggle input form
-                        self.input
-                            .set_new_titan(&self.tab_action, &uri, Some(title));
+                        self.input.set_new_titan(&self.tab_action, uri, Some(title));
 
                         // Update meta
                         self.meta.set_status(status).set_title(title);
