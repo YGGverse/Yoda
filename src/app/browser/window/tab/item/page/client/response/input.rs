eb90a6016244bff5184d8136556e6c834c7d1cac
yggverse <yggverse@project>
2025-01-19 11:31:35 +0200

    update titan input struct

--- src/app/browser/window/tab/item/page/client/response/input.rs
+++ src/app/browser/window/tab/item/page/client/response/input.rs
@@ -1,7 +1,20 @@
-use gtk::glib::{GString, Uri};
+use gtk::{
+    gio::{Cancellable, IOStream},
+    glib::{GString, Uri},
+};
 
 pub enum Input {
-    Response { base: Uri, title: GString },
-    Sensitive { base: Uri, title: GString },
-    Titan { base: Uri },
+    Response {
+        base: Uri,
+        title: GString,
+    },
+    Sensitive {
+        base: Uri,
+        title: GString,
+    },
+    Titan {
+        base: Uri,
+        cancellable: Cancellable,
+        stream: IOStream,
+    },
 }
