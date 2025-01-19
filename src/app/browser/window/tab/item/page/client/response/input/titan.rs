89469c2aa3952739d8890b97c9082243dec2aa31
yggverse <yggverse@project>
2025-01-19 13:49:42 +0200

    draft Titan response send action

--- src/app/browser/window/tab/item/page/client/response/input/titan.rs
+++ src/app/browser/window/tab/item/page/client/response/input/titan.rs
@@ -1,6 +1,23 @@
-use gtk::gio::{Cancellable, IOStream};
+use gtk::{
+    gio::{Cancellable, IOStream},
+    glib::{Bytes, Error, Priority},
+    prelude::{IOStreamExt, OutputStreamExt},
+};
 
 pub struct Titan {
     cancellable: Cancellable,
     stream: IOStream,
+}
+
+impl Titan {
+    // Actions
+
+    pub fn send(&self, data: Bytes, callback: impl FnOnce(Result<isize, Error>) + 'static) {
+        self.stream.output_stream().write_bytes_async(
+            &data,
+            Priority::DEFAULT,
+            Some(&self.cancellable),
+            callback,
+        )
+    }
 }
