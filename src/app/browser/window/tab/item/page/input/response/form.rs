8059acd0e8a20078ea7836f8dbe25d587c018bda
yggverse <yggverse@project>
2025-01-22 12:26:20 +0200

    fix `TextView` size allocation (bug #635)

--- src/app/browser/window/tab/item/page/input/response/form.rs
+++ src/app/browser/window/tab/item/page/input/response/form.rs
@@ -40,6 +40,7 @@ impl Form {
             .build();
 
         text_view.insert_action_group("spelling", Some(&adapter));
+        text_view.set_size_request(-1, 38); // @TODO [#635](https://gitlab.gnome.org/GNOME/pygobject/-/issues/635)
 
         // Init events
         text_view.buffer().connect_changed(move |_| {
