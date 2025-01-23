92550a2ccc3eed0101353e963ab9949aa17d264e
yggverse <yggverse@project>
2025-01-23 10:25:16 +0200

    lowercase request subject to fix scheme detection

--- src/app/browser/window/tab/item/page/navigation/request/widget/primary_icon.rs
+++ src/app/browser/window/tab/item/page/navigation/request/widget/primary_icon.rs
@@ -22,28 +22,30 @@ pub enum PrimaryIcon<'a> {
 }
 
 pub fn from(request: &str) -> PrimaryIcon {
-    if request.starts_with("download:") {
+    let prefix = request.to_lowercase();
+
+    if prefix.starts_with("download:") {
         return PrimaryIcon::Download {
             name: "document-save-symbolic",
             tooltip: "Download",
         };
     }
 
-    if request.starts_with("source:") {
+    if prefix.starts_with("source:") {
         return PrimaryIcon::Source {
             name: "applications-system-symbolic",
             tooltip: "Source view",
         };
     }
 
-    if request.starts_with("gemini:") {
+    if prefix.starts_with("gemini:") {
         return PrimaryIcon::Gemini {
             name: "channel-secure-symbolic",
             tooltip: ("Guest session", "User session"),
         };
     }
 
-    if request.starts_with("titan:") {
+    if prefix.starts_with("titan:") {
         return PrimaryIcon::Titan {
             name: "document-send-symbolic",
             tooltip: ("Guest titan input", "User titan input"),
