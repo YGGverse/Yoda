ae45108d7fd5b54830a501445ac11aace8c74f86
yggverse <yggverse@project>
2025-02-01 16:56:03 +0200

    change headers palette

--- src/app/browser/window/tab/item/page/content/text/gemini/reader/tag/h3.rs
+++ src/app/browser/window/tab/item/page/content/text/gemini/reader/tag/h3.rs
@@ -2,7 +2,7 @@ use gtk::{TextTag, WrapMode};
 
 pub fn new() -> TextTag {
     TextTag::builder()
-        .foreground("#3a944a") // @TODO optional
+        .foreground("#c88800") // @TODO optional
         .scale(1.2)
         .sentence(true)
         .weight(400)
