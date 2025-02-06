f3a4727a27f3b4309e92d5b4a9b7e27cb080b3d4
yggverse <yggverse@project>
2025-02-06 20:11:56 +0200

    update description

--- src/app/browser/window/tab/item/page/input/titan/header.rs
+++ src/app/browser/window/tab/item/page/input/titan/header.rs
@@ -31,7 +31,7 @@ impl Header {
         // Init main widget
         let alert_dialog = AlertDialog::builder()
             .heading("Options")
-            .body("Custom response header")
+            .body("Customize response header")
             .close_response(RESPONSE_CANCEL.0)
             .default_response(RESPONSE_APPLY.0)
             .extra_child(&form.g_box)
