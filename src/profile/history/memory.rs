f09bb905f97a71c60cf8da942465ad06e21c03ea
yggverse <yggverse@project>
2025-03-11 12:48:10 +0200

    make search case insensitive

--- src/profile/history/memory.rs
+++ src/profile/history/memory.rs
@@ -78,14 +78,19 @@ impl Memory {
     }
 
     /// Get unordered Items vector contains `request`
+    /// * this function is case insensitive
     pub fn contains_request(&self, request: &str, limit: Option<usize>) -> Vec<Item> {
         let mut items: Vec<Item> = Vec::new();
         for (i, item) in self.0.iter().enumerate() {
             if limit.is_some_and(|l| i > l) {
                 break;
             }
-            if item.request.contains(request)
-                || item.title.as_ref().is_some_and(|t| t.contains(request))
+            let p = request.to_lowercase();
+            if item.request.to_lowercase().contains(&p)
+                || item
+                    .title
+                    .as_ref()
+                    .is_some_and(|t| t.to_lowercase().contains(&p))
             {
                 items.push(item.clone())
             }
