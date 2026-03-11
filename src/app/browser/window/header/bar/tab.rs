bf039dd9471324c34aa7788c6e8a917f36092a74
yggverse <yggverse@project>
2026-03-11 02:48:51 +0200

    show `x` button at left by respecting the env settings

--- src/app/browser/window/header/bar/tab.rs
+++ src/app/browser/window/header/bar/tab.rs
@@ -14,8 +14,12 @@ impl Tab for TabBar {
     fn tab(window_action: &Rc<WindowAction>, view: &TabView) -> Self {
         TabBar::builder()
             .autohide(false)
-            .expand_tabs(false)
             .end_action_widget(&Button::append(window_action)) // @TODO find solution to append after tabs
+            .expand_tabs(false)
+            .inverted(gtk::Settings::default().is_some_and(|s| {
+                s.gtk_decoration_layout()
+                    .is_some_and(|l| l.starts_with("close"))
+            })) // show `x` button at left by respecting the env settings
             .view(view)
             .build()
     }
